ae70763d7f43cc17fa5ba9a6851025e0b55bf759
wangjianming <wangjianming@sanniuben.com>
2019-08-08 19:00:50 +0800

    设计模式列子

---
+++ src/main/java/com/jm/design/adapter/ObjectAdapterTest.java
@@ -0,0 +1,26 @@
+package com.jm.design.adapter;
+
+class ObjectAdapter implements Target{
+
+    private Adaptee adaptee;
+
+    public ObjectAdapter(Adaptee adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    public void request() {
+        adaptee.specificRequest();
+    }
+}
+
+/**
+ * 对象适配器模式
+ */
+public class ObjectAdapterTest {
+    public static void main(String[] args) {
+        System.out.println("对象适配器模式测试：");
+        Adaptee adaptee = new Adaptee();
+        Target target = new ObjectAdapter(adaptee);
+        target.request();
+    }
+}
